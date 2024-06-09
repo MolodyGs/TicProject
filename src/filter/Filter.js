c2e98cd5fe1f4ee1ab4ffb4fb11f6094ae14b6b0
Joaquin Pinto <joaquin-pinto@alumnos.ucn.cl>
2024-06-08 20:31:02 -0400

    Pequeño avance

--- src/filter/Filter.js
+++ src/filter/Filter.js
@@ -1,22 +1,40 @@
-import { MinePlan } from "../minePlan/MinePlan.js";
+export class Filter {
+  constructor() {}
 
-let minePlan = new MinePlan();
+  async loadFilter(txt, period, filterType) {
+    const filteredData = await minePlan.loadPeriod(txt, period);
 
-export class Filter{
-  constructor() {
-    
-  }
+    if (filterType === -1) {
+      return filteredData;
+    }
 
-  //Si filterType = -1, entonces es que no se requiere ningún filtro.
-  async loadFilter( txt, period, filterType){
-    
-    //Obtenemos los datos desde MinePlan para ser filtrados
-    const filtedData = await minePlan.loadPeriod(txt, period);
+    const typeBConditions = (x, z) => {
+      // Aquí van las condiciones para los bloques de tipo B
+      if (
+        (z == 1 && ((x <= 2) || (x >= 6 && x <= 9) || (x >= 13 && x <= 14) || (x >= 18 && x <= 19) || (x >= 23 && x <= 25) || (x >= 29 && x <= 30) || (x >= 35))) ||
+        (z == 2 && ((x <= 2) || (x >= 6 && x <= 8) || (x >= 13 && x <= 14) || (x >= 18 && x <= 19) || (x >= 23 && x <= 24) || (x >= 29 && x <= 30) || (x >= 34))) ||
+        (z == 3 && ((x <= 1) || (x >= 5 && x <= 8) || (x >= 12 && x <= 14) || (x >= 17 && x <= 18) || (x >= 22 && x <= 23) || (x >= 28 && x <= 29) || (x >= 35))) ||
+        (z == 4 && ((x <= 1) || (x >= 5 && x <= 7) || (x >= 12 && x <= 13) || (x >= 17 && x <= 18) || (x >= 22 && x <= 23) || (x >= 27 && x <= 28) || (x >= 35))) ||
+        (z == 5 && ((x >= 5 && x <= 7) || (x >= 11 && x <= 13) || (x >= 17 && x <= 18) || (x >= 22 && x <= 23) || (x >= 27 && x <= 28) || (x == 32) || (x >= 35))) ||
+        (z == 6 && ((x >= 4 && x <= 6) || (x >= 11 && x <= 13) || (x == 17) || (x >= 21 && x <= 22) || (x >= 27 && x <= 28) || (x == 32) || (x >= 35))) ||
+        (z == 7 && ((x >= 4 && x <= 5) || (x >= 11 && x <= 13) || (x >= 16 && x <= 17) || (x >= 20 && x <= 22) || (x >= 26 && x <= 27) || (x == 31) || (x == 35))) ||
+        (z == 8 && ((x == 1) || (x >= 3 && x <= 5) || (x == 7) || (x >= 11 && x <= 12) || (x == 16) || (x >= 20 && x <= 21) || (x >= 26 && x <= 27) || (x == 31) || (x >= 34 && x <= 35))) ||
+        (z == 9 && ((x <= 6) || (x >= 10 && x <= 16) || (x >= 19 && x <= 22) || (x >= 26 && x <= 28) || (x == 31) || (x >= 34 && x <= 35))) ||
+        (z == 10 && ((x <= 4) || (x >= 9 && x <= 14) || (x >= 19 && x <= 20) || (x >= 25 && x <= 27) || (x == 31) || (x == 33))) ||
+        (z == 11 && ((x <= 3) || (x >= 8 && x <= 13) || (x >= 17 && x <= 20) || (x >= 24 && x <= 26) || (x >= 30 && x <= 32))) ||
+        (z == 12)
+      ) {
+        return true;
+      }
+      return false;
+    };
 
-    //...
-    //Lógica para el filtro de datos
-    //...
-    
-    return filtedData;
+    if (filterType === "A") {
+      return filteredData.filter(cube => !typeBConditions(cube[0], cube[2]));
+    } else if (filterType === "B") {
+      return filteredData.filter(cube => typeBConditions(cube[0], cube[2]));
+    }
+
+    return filteredData;
   }
 }
--- src/visualization/Visualization.js
+++ src/visualization/Visualization.js
@@ -2,130 +2,104 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { render } from 'react-dom';
 
-export class Visualization{
+export class Visualization {
+  loadVisualization = (cubesData) => {
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color('#FFFFFF');
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-    loadVisualization = (cubesData) => {
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(1920, window.innerHeight);
+    if (document.querySelector("body canvas")) {
+      document.querySelector("body canvas").parentNode.removeChild(document.querySelector("body canvas"));
+    }
+    document.body.appendChild(renderer.domElement);
 
-        const scene = new THREE.Scene();
-        scene.background = new THREE.Color('#FFFFFF');
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const controls = new OrbitControls(camera, renderer.domElement);
+    const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+    const cubesToScreen = [];
+    const gridSize = 8;
+    const spacing = 1;
+    const offset = (gridSize - 1) * spacing / 2;
 
-        const renderer = new THREE.WebGLRenderer();
-        renderer.setSize(1920, window.innerHeight);
-        if(document.querySelector("body canvas")){
-          document.querySelector("body canvas").parentNode.removeChild(document.querySelector("body canvas"));
+    const greyScale = () => {
+      let min = 99999;
+      let max = 0;
+      cubesData.forEach(cube => {
+        const value = parseFloat(cube[4]);
+        if (value > max) {
+          max = value;
+        } else if (value < min) {
+          min = value;
         }
-        document.body.appendChild(renderer.domElement);
+      });
+      return [min, max];
+    };
 
-        // Añade OrbitControls para controlar la cámara
-        const controls = new OrbitControls(camera, renderer.domElement);
+    const minValue = greyScale()[0];
+    const maxValue = greyScale()[1];
 
-        // Crea geometría reutilizable para los cubos
-        const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+    cubesData.forEach(cube => {
+      const solidMaterial = new THREE.MeshBasicMaterial({ color: "red" });
+      const wireframeMaterial = new THREE.MeshBasicMaterial({ color: '#000000', wireframe: true });
+      const solidCube = new THREE.Mesh(boxGeometry, solidMaterial);
+      const wireframeCube = new THREE.Mesh(boxGeometry, wireframeMaterial);
+      const cubeGroup = new THREE.Group();
+      cubeGroup.add(solidCube);
+      cubeGroup.add(wireframeCube);
 
-        // Lista para almacenar cubos y sus tipos
-        const cubesToScreen = [];
+      cubeGroup.type = cube[3] === "B" ? "B" : "A";
 
-        // Dimensiones de la cuadrícula y espacio total
-        const gridSize = 8;  // Tamaño de la cuadrícula (10x10x10 = 1000 cubos)
-        const spacing = 1;  // Espacio entre cubos
-        const offset = (gridSize - 1) * spacing / 2;  // Para centrar la cuadrícula
+      cubeGroup.position.set(
+        cube[0] * spacing,
+        cube[1] * spacing,
+        cube[2] * spacing
+      );
 
-        const greyScale = () => {
-            let min = 99999;
-            let max = 0;
-            cubesData.forEach(cube => {
-                const value = parseFloat(cube[4]);
-                if(value > max){
-                    max = value;
-                }else if(value < min){
-                    min = value;
-                }
-            });
-            return [min, max];
-        }
+      cubesToScreen.push(cubeGroup);
+      scene.add(cubeGroup);
+    });
 
-        const minValue = greyScale()[0];
-        const maxValue = greyScale()[1];
+    camera.position.z = 30;
 
-        cubesData.forEach(cube => {
-            // console.log("Cargando cubo...")
-            const solidMaterial = new THREE.MeshBasicMaterial({ color: "red" });
-
-            // Material para el wireframe (borde negro)
-            const wireframeMaterial = new THREE.MeshBasicMaterial({ color: '#000000', wireframe: true });
-
-            const solidCube = new THREE.Mesh(boxGeometry, solidMaterial);
-            const wireframeCube = new THREE.Mesh(boxGeometry, wireframeMaterial);
-
-            // Agrupa ambos para crear el efecto de bordes negros
-            const cubeGroup = new THREE.Group();
-            cubeGroup.add(solidCube);
-            cubeGroup.add(wireframeCube);
-
-            cubeGroup.type = "hierro";  // Asigna el tipo al grupo
-
-            // Posiciona el cubo en la cuadrícula 3D
-            cubeGroup.position.set(
-                cube[0] * spacing,
-                cube[1] * spacing,
-                cube[2] * spacing
-            );
-
-            cubesToScreen.push(cubeGroup);  // Añade el grupo a la lista
-            scene.add(cubeGroup);  // Añade el grupo a la escena
-        });
-
-        // Posiciona la cámara para tener una vista amplia
-        camera.position.z = 30;
-
-        // Función de animación
-        function animate() {
-            requestAnimationFrame(animate);
-            controls.update();  // Permite la interacción de la cámara
-            renderer.render(scene, camera);  // Renderiza la escena
-        }
-
-        animate();  // Inicia la animación
-
-        // Filtra los cubos según el tipo
-        // function filterCubes(type) {
-        //     cubes.forEach(cube => {
-        //         if (type === 'todos' || cube.type === type) {
-        //             cube.visible = true;  // Muestra el cubo si coincide el tipo
-        //         } else {
-        //             cube.visible = false;  // Oculta el cubo si no coincide el tipo
-        //         }
-        //     });
-        // }
-
-        // Añade botones para filtrar cubos
-        const buttonContainer = document.createElement('div');
-        buttonContainer.style.position = 'absolute';
-        buttonContainer.style.top = '10px';
-        buttonContainer.style.right = '10px';
-
-        const buttonAll = document.createElement('button');
-        buttonAll.innerText = 'Todos';
-        buttonAll.onclick = () => filterCubes('todos');
-
-        const buttonIron = document.createElement('button');
-        buttonIron.innerText = 'Hierro';
-        buttonIron.onclick = () => filterCubes('hierro');
-
-        const buttonGold = document.createElement('button');
-        buttonGold.innerText = 'Oro';
-        buttonGold.onclick = () => filterCubes('oro');
-
-        const buttonCopper = document.createElement('button');
-        buttonCopper.innerText = 'Cobre';
-        buttonCopper.onclick = () => filterCubes('cobre');
-
-        buttonContainer.appendChild(buttonAll);
-        buttonContainer.appendChild(buttonIron);
-        buttonContainer.appendChild(buttonGold);
-        buttonContainer.appendChild(buttonCopper);
-        document.body.appendChild(buttonContainer);
+    function animate() {
+      requestAnimationFrame(animate);
+      controls.update();
+      renderer.render(scene, camera);
     }
 
+    animate();
+
+    const filterCubes = (type) => {
+      cubesToScreen.forEach(cube => {
+        if (type === 'todos' || cube.type === type) {
+          cube.visible = true;
+        } else {
+          cube.visible = false;
+        }
+      });
+    };
+
+    const buttonContainer = document.createElement('div');
+    buttonContainer.style.position = 'absolute';
+    buttonContainer.style.top = '10px';
+    buttonContainer.style.right = '10px';
+
+    const buttonAll = document.createElement('button');
+    buttonAll.innerText = 'Todos';
+    buttonAll.onclick = () => filterCubes('todos');
+
+    const buttonA = document.createElement('button');
+    buttonA.innerText = 'A';
+    buttonA.onclick = () => filterCubes('A');
+
+    const buttonB = document.createElement('button');
+    buttonB.innerText = 'B';
+    buttonB.onclick = () => filterCubes('B');
+
+    buttonContainer.appendChild(buttonAll);
+    buttonContainer.appendChild(buttonA);
+    buttonContainer.appendChild(buttonB);
+    document.body.appendChild(buttonContainer);
+  }
 }
